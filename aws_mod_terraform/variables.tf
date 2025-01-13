2f2b4fd8b5e050c3f91661e5276891cbe8dc6ec1
Alejandro Ingles <aleingmar@alum.us.es>
2025-01-13 18:04:02 +1000

    sin errores

---
+++ aws_mod_terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_access_key" {
+    description = "La clave de acceso de AWS"
+    type        = string
+}
+
+variable "aws_secret_key" {
+    description = "La clave secreta de AWS"
+    type        = string
+}
+
+variable "aws_session_token" {
+    description = "El token de sesión de AWS"
+    type        = string
+}
